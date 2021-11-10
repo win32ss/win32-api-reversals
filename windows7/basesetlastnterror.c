0505df7af131732e8fcc4f9ad880efa7360ed503
win32ss <93808768+win32ss@users.noreply.github.com>
2021-11-09 19:36:05 -0500

    Update basesetlastnterror.c

--- windows7/basesetlastnterror.c
+++ windows7/basesetlastnterror.c
@@ -1,8 +1,8 @@
 /* win32 - November 9 2021
  This has been in NT forever, but was only exported starting with Windows 7.
- And I have encountered applications that do call it, albeit they are rare. */
+ And I think I have encountered applications that do call it, albeit they are rare. */
 
-ULONG BaseSetLastNtError(NTSTATUS Status)
+ULONG BaseSetLastNTError(NTSTATUS Status) // Export names are case-sensitive. And the standard is to type "Nt" instead of "NT". This is an exception.
 {
   ULONG DosError;
 
