e5c03d94ed850399287aa666a6f068b966d1fde2
win32ss <93808768+win32ss@users.noreply.github.com>
2022-03-07 15:20:49 -0500

    Add files via upload

---
+++ windows7/oleapi.c
@@ -0,0 +1,100 @@
+
+#include <windows.h>
+#include <winternl.h>
+
+
+BOOLEAN g_cMTAInits;
+DWORD gdwMainThreadId;
+
+int __stdcall DllMain()
+{
+	return 0;
+}
+
+HRESULT WINAPI CoGetApartmentType(APTTYPE *pAptType, APTTYPEQUALIFIER *pAptQualifier)
+/* 
+    win32 - March 7 2021
+	An important OLE function introduced in Windows 7, but should be usable in NT 4 and above.
+*/
+{
+  HRESULT Status; 
+  BOOLEAN MTAInit; 
+  DWORD *ReservedForOle; 
+  DWORD CurrentThreadId; 
+  DWORD OleFlags; 
+  APTTYPE AptTypeTemp; 
+  APTTYPEQUALIFIER AptQualifierTemp; 
+  TEB *CurrentThreadInfo;
+
+  Status = S_OK;
+  if ( !pAptType || !pAptQualifier )
+    return E_INVALIDARG;
+  *pAptType = APTTYPE_CURRENT;
+  *pAptQualifier = APTTYPEQUALIFIER_NONE;
+  MTAInit = FALSE;
+  CurrentThreadInfo = NtCurrentTeb();
+  ReservedForOle = CurrentThreadInfo->ReservedForOle;
+  if ( g_cMTAInits )
+    MTAInit = TRUE;
+  // The following is a GetCurrentThreadId() replacement.
+  // NtCurrentTeb()->UniqueThread.ClientId or NtCurrentTeb() + 48h (AMD64) or NtCurrentTeb() + 24h (X86)
+  CurrentThreadId = (DWORD)CurrentThreadInfo->Reserved1[9];
+
+  if ( !ReservedForOle )
+  {
+    if ( MTAInit )
+      goto ImplicitMTA;
+    return CO_E_NOTINITIALIZED;
+  }
+  OleFlags = ReservedForOle[5];
+  if ( (OleFlags & 0x800) == 0 )
+  {
+    if ( (OleFlags & 0x80) != 0 )
+    {
+      AptTypeTemp = APTTYPE_STA;
+      if ( CurrentThreadId == gdwMainThreadId )
+        AptTypeTemp = APTTYPE_MAINSTA;
+      *pAptType = AptTypeTemp;
+      if ( (OleFlags & 0x400000) != 0 )
+        *pAptQualifier = APTTYPEQUALIFIER_APPLICATION_STA;
+   
+      if ( (OleFlags & 0x40000000) != 0 )
+        *pAptQualifier = APTTYPEQUALIFIER_RESERVED_1;
+      return Status;
+    }
+    if ( (OleFlags & 0x1100) != 0 )
+    {
+      *pAptType = APTTYPE_MTA;
+      return Status;
+    }
+    if ( MTAInit )
+    {
+ImplicitMTA:
+      *pAptType = APTTYPE_MTA;
+      *pAptQualifier = APTTYPEQUALIFIER_IMPLICIT_MTA;
+      return Status;
+    }
+    return CO_E_NOTINITIALIZED;
+  }
+  AptQualifierTemp = APTTYPEQUALIFIER_NA_ON_MTA;
+  *pAptType = APTTYPE_NA;
+  if ( (OleFlags & 0x80) != 0 )
+  {
+    AptQualifierTemp = APTTYPEQUALIFIER_NA_ON_STA;
+    if ( CurrentThreadId == gdwMainThreadId )
+      AptQualifierTemp = APTTYPEQUALIFIER_NA_ON_MAINSTA;
+    *pAptQualifier = AptQualifierTemp;
+	return Status;
+  }
+  if ( (OleFlags & 0x1100) != 0 )
+  {
+    *pAptQualifier = AptQualifierTemp;
+    return Status;
+  }
+  if ( MTAInit )
+  {
+    *pAptQualifier = APTTYPEQUALIFIER_NA_ON_IMPLICIT_MTA;
+    return Status;
+  }
+  return E_FAIL;
+}
