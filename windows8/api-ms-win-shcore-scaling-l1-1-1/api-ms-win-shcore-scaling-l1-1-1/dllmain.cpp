bcf3d9ec879a63652f1a27a1a0dc7f9928ab62ef
win32ss <93808768+win32ss@users.noreply.github.com>
2022-04-01 10:19:53 -0400

    Add files via upload
    improved function

--- windows8/api-ms-win-shcore-scaling-l1-1-1/api-ms-win-shcore-scaling-l1-1-1/dllmain.cpp
+++ windows8/api-ms-win-shcore-scaling-l1-1-1/api-ms-win-shcore-scaling-l1-1-1/dllmain.cpp
@@ -22,7 +22,12 @@ HRESULT WINAPI GetScaleFactorForMonitor
 ( IN HMONITOR       hMon,
   OUT DEVICE_SCALE_FACTOR *pScale)
 {
-	*pScale = SCALE_100_PERCENT;
+	HDC hDC;
+	INT index;
+	hDC = GetDC(NULL);
+	index = GetDeviceCaps(hDC, LOGPIXELSX);
+	*pScale = (DEVICE_SCALE_FACTOR)((96/index) * 100);
+	CloseHandle(hDC);
 	return S_OK;
 }
 
