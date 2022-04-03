d2b61cedfbe4786b4f9c793ef55658842f3cfae4
win32ss <93808768+win32ss@users.noreply.github.com>
2022-04-03 15:32:21 -0400

    Update dllmain.cpp

--- windows8/api-ms-win-shcore-scaling-l1-1-1/api-ms-win-shcore-scaling-l1-1-1/dllmain.cpp
+++ windows8/api-ms-win-shcore-scaling-l1-1-1/api-ms-win-shcore-scaling-l1-1-1/dllmain.cpp
@@ -27,7 +27,6 @@ HRESULT WINAPI GetScaleFactorForMonitor
 	hDC = GetDC(NULL);
 	index = GetDeviceCaps(hDC, LOGPIXELSX);
 	*pScale = (DEVICE_SCALE_FACTOR)((96/index) * 100);
-	CloseHandle(hDC);
 	return S_OK;
 }
 
